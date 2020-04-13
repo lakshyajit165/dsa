7c9c19ba490b865f889544377615b8312e2505f2
Lakshyajit <lakshyajit165@gmail.com>
2020-04-13 20:29:48 +0530

    leetcode problem

---
+++ extras/contiguous_array/src/contiguous_array.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int maxlen = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count = count + (nums[i] == 1 ? 1 : -1);
+            if (map.containsKey(count)) {
+                maxlen = Math.max(maxlen, i - map.get(count));
+            } else {
+                map.put(count, i);
+            }
+        }
+        return maxlen;
+    }
+}
+
+public class contiguous_array {
+
+    public static void main(String[] args){
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        s.nextLine();
+
+        int nums[] = new int[n];
+        for(int i = 0; i<n; i++){
+            nums[i] = s.nextInt();
+            //s.nextLine();
+        }
+
+        Solution sn = new Solution();
+        System.out.println(sn.findMaxLength(nums));
+
+    }
+}
