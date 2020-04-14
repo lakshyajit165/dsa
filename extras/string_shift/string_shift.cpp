94bb08ceb03057d5c26925a9568a3567bea11538
Lakshyajit <lakshyajit165@gmail.com>
2020-04-15 00:19:53 +0530

    string shift day 14 leetcode

---
+++ extras/string_shift/string_shift.cpp
@@ -0,0 +1,93 @@
+/*
+
+
+You are given a string s containing 
+lowercase English letters, and a matrix shift, where shift[i] = [direction, amount]:
+
+direction can be 0 (for left shift) or 1 (for right shift). 
+amount is the amount by which string s is to be shifted.
+A left shift by 1 means remove the first character of s and append it to the end.
+Similarly, a right shift by 1 means remove the last 
+character of s and add it to the beginning.
+Return the final string after all operations.
+
+ 
+
+Example 1:
+
+Input: s = "abc", shift = [[0,1],[1,2]]
+Output: "cab"
+Explanation: 
+[0,1] means shift to left by 1. "abc" -> "bca"
+[1,2] means shift to right by 2. "bca" -> "cab"
+Example 2:
+
+Input: s = "abcdefg", shift = [[1,1],[1,1],[0,2],[1,3]]
+Output: "efgabcd"
+Explanation:  
+[1,1] means shift to right by 1. "abcdefg" -> "gabcdef"
+[1,1] means shift to right by 1. "gabcdef" -> "fgabcde"
+[0,2] means shift to left by 2. "fgabcde" -> "abcdefg"
+[1,3] means shift to right by 3. "abcdefg" -> "efgabcd"
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s only contains lower case English letters.
+1 <= shift.length <= 100
+shift[i].length == 2
+0 <= shift[i][0] <= 1
+0 <= shift[i][1] <= 100
+
+
+
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string stringShift(string s, vector<vector<int>>& shift) {
+        
+        for(int i = 0; i < shift.size(); i++){
+            if(shift[i][0] == 0){
+                leftRotate(s, shift[i][1]);
+            }else{
+                rightRotate(s, shift[i][1]);
+            }
+        }
+        
+        return s;
+    }
+    
+    void leftRotate(string &s, int d){
+        
+        reverse(s.begin(), s.begin()+d);
+        reverse(s.begin()+d, s.end());
+        reverse(s.begin(), s.end());
+        
+    }
+    
+    void rightRotate(string &s, int d){
+        
+        leftRotate(s, s.length()-d);
+    }
+};
+
+int main(){
+
+    Solution s;
+
+    vector<vector<int> > vect{ { 1, 1 }, 
+                               { 4, 1 }, 
+                               { 0, 2 },
+                               { 1, 3} }; 
+    string str = "abcdefg";
+    
+    cout<<s.stringShift(str, vect)<<endl;
+
+    return 0;
+}
