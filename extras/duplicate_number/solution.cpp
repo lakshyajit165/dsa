27687c1ed1f0800febb6b285bf81c7998960a28d
Lakshyajit <lakshyajit165@gmail.com>
2020-06-26 19:01:33 +0530

    june leetcoding challenge

---
+++ extras/duplicate_number/solution.cpp
@@ -0,0 +1,66 @@
+/*
+Find the Duplicate Number
+
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive),
+ prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+Example 1:
+
+Input: [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: [3,1,3,4,2]
+Output: 3
+Note:
+
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once.
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        if(nums.empty())
+           return 0;
+        
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        
+        fast = nums[0];
+        
+        while(fast != slow) {
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        
+        return fast;
+    }
+};
+int main() {
+
+    int ele;
+    vector<int> nums;
+
+    while(cin.peek() != '\n' and cin.peek() != EOF){
+        cin>>ele;
+        nums.push_back(ele);
+    }
+
+    Solution s;
+    cout<<s.findDuplicate(nums)<<endl;
+}
